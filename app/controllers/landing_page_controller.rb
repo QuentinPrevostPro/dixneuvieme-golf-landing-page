c88648a56c756467a6c68efaa2ddd7df281e45ad
QuentinPrevost2 <prevostq@gmail.com>
2019-05-17 11:58:42 +0200

    Google Sheet connection added

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,4 +1,9 @@
 class LandingPageController < ApplicationController
   def index
+    @session = GoogleDrive::Session.from_config("config.json")
+    @ws = @session.spreadsheet_by_key("1MKrYhqrlNSb0sWRsqeBjeDIK9KEMmhI3sYxHSpxh-mg").worksheets[0]
+    @ws[@ws.num_rows+1,2] = @ws.num_rows+1
+    @ws[@ws.num_rows,3] = @ws.num_rows
+    @ws.save
   end
 end
