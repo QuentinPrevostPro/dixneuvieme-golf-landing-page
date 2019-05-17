b21d6b04dd897193e6dae2213e0742e7e905e5e8
QuentinPrevost2 <prevostq@gmail.com>
2019-05-17 14:24:54 +0200

    subscribe form working

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,9 +1,14 @@
 class LandingPageController < ApplicationController
   def index
+  end
+  def create
+    puts "<><><><><><><><><><><><><><><><><>"
+    puts params
+    puts "<><><><><><><><><><><><><><><><><>"
     @session = GoogleDrive::Session.from_config("config.json")
     @ws = @session.spreadsheet_by_key("1MKrYhqrlNSb0sWRsqeBjeDIK9KEMmhI3sYxHSpxh-mg").worksheets[0]
-    @ws[@ws.num_rows+1,2] = @ws.num_rows+1
-    @ws[@ws.num_rows,3] = @ws.num_rows
+    @ws[@ws.num_rows+1,2] = params[:email]
+    @ws[@ws.num_rows,3] = "#{DateTime.now.day > 9 ? "#{DateTime.now.day}" : "0#{DateTime.now.day}"}/#{DateTime.now.month > 9 ? "#{DateTime.now.month}" : "0#{DateTime.now.month}"}/#{DateTime.now.year}"
     @ws.save
   end
 end
